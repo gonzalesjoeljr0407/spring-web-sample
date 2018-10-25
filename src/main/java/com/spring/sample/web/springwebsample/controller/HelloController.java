d76c211a6cd4500ed2ac56077b6eec2c2089de46
Joel Gonzales <gonzalesjoeljr0407@gmail.com>
2018-10-25 06:25:52 +0000

    add another page

---
+++ src/main/java/com/spring/sample/web/springwebsample/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.spring.sample.web.springwebsample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class HelloController {
+    
+    @GetMapping
+    public String getLogin() {
+        return "login";
+    }
+}
