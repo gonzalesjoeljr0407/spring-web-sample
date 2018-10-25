0843e352cff484ce4ac9f089b7253a4bb3e20fd4
Joel Gonzales <gonzalesjoeljr0407@gmail.com>
2018-10-25 08:31:48 +0000

    update

--- src/main/java/com/spring/sample/web/springwebsample/model/entity/Product.java
+++ src/main/java/com/spring/sample/web/springwebsample/model/entity/Product.java
@@ -2,6 +2,8 @@ package com.spring.sample.web.springwebsample.model.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,6 +13,7 @@ public class Product {
     
     @Id
     @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(name="product_name")
     private String productName;
