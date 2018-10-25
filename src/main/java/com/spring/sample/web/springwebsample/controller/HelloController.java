d1e3dfd31fec97b99ab3b25475406a846114729f
Joel Gonzales <gonzalesjoeljr0407@gmail.com>
2018-10-25 06:55:00 +0000

    update login

--- src/main/java/com/spring/sample/web/springwebsample/controller/HelloController.java
+++ src/main/java/com/spring/sample/web/springwebsample/controller/HelloController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.sample.web.springwebsample.model.Hello;
+
 @Controller
 @RequestMapping("/login")
 public class HelloController {
     
     @GetMapping("/loginPage/{value}")
     public String getLogin(@PathVariable("value") String value, Model model) {
+        Hello hello = new Hello("hello", "");
+        model.addAttribute("hello", hello);
         model.addAttribute("value", value);
         return "login";
     }
---
+++ src/main/java/com/spring/sample/web/springwebsample/model/Hello.java
@@ -0,0 +1,28 @@
+package com.spring.sample.web.springwebsample.model;
+
+public class Hello {
+    
+    public String username;
+    public String password;
+    
+    public Hello(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
